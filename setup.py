6e589f62c123c9877921728826626f3ef7004875
Dr. Aleksei Krasnov <dr.aleksei.krasnov@gmail.com>
2026-01-30 10:17:17 +0100

    chore: updated requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,25 @@
 import setuptools
 
+def read_requirements():
+    """Read the requirements.txt file and return a list of dependencies."""
+    with open("requirements.txt", "r", encoding="utf-8") as fh:
+        return fh.read().splitlines()
 
 setuptools.setup(
     name="MolNexTR",
-    version="1.0.0",
+    version="1.0.2",
     description="MolNexTR, a novel graph generation model",
     entry_points={
         "console_scripts": ["molnextr = MolNexTR.molnextr:main"],
     },
-    url="https://github.com/Kohulan/MolNexTR",
+    url="https://github.com/CYF2000127/MolNexTR",
     packages=setuptools.find_packages(),
     license="Apache License 2.0",
-    install_requires=[
-        "pytorch-lightning",
-        "opencv-python",
-        "pystow",
-        "pillow",
-        "pyparsing",
-        "six",
-        "albumentations==1.1.0",
-        "SmilesPE",
-        "timm==0.4.12",
-        "pyonmttok==1.37.1",
-        "OpenNMT-py==2.2.0",
-        "matplotlib",
-        "pandas",
-        "rdkit",
-    ],
+    install_requires=read_requirements(),
     package_data={"MolNexTR": ["decoding/*.*", "indigo/*.*", "models/*.*","vocab/*.*"]},
     classifiers=[
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: Apache License 2.0",
         "Operating System :: OS Independent",
