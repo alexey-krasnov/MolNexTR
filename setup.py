5c93deba2a00ace22ac02d0e0afaebd713ae49cd
Dr. Aleksei Krasnov <dr.aleksei.krasnov@gmail.com>
2026-01-30 11:09:07 +0100

    chore: improved setup.py to read __version__ from __init__

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ def read_requirements():
     with open("requirements.txt", "r", encoding="utf-8") as fh:
         return fh.read().splitlines()
 
+# Read the version from __init__.py
+version = {}
+with open("molnextr/__init__.py", "r", encoding="utf-8") as fh:
+    for line in fh:
+        if line.startswith("__version__"):
+            exec(line, version)
+            break
+
 setuptools.setup(
     name="molnextr",
-    version="1.0.2",
+    version=version["__version__"],  # Use the version from version.py
     description="molnextr, a novel graph generation model",
     entry_points={
         "console_scripts": ["molnextr = molnextr.molnextr:main"],
